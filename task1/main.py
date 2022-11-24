f734a262249cc61d45b77e01ec02be8a1413ebd9
jbizLT <pozolotusiko@mail.ru>
2022-11-25 00:39:59 +0300

    [feature] file output

--- task1/main.py
+++ task1/main.py
@@ -9,10 +9,10 @@ files = 2
 f_probability = open("stats_probability.txt", "w")
 f_deviation = open("stats_deviation.txt", "w")
 f_start = open("stats_start.txt", "w")
+f_reviewers = open("stats_reviewers.txt", "w")
 
 
 def hypothesis_1_vse(error_guys):
-    global file
     avg_in_one_file = []
     kb_otk = []
     is_avg_by_people = {}
@@ -37,7 +37,8 @@ def hypothesis_1_vse(error_guys):
         kb_otk = []
     f_deviation.write("Deviation stats:\n\n")
     for human in is_avg_by_people:
-        f_deviation.write(str(human) + " " + str(is_avg_by_people.get(human)))
+        f_deviation.write("------------------------------------------------------\n")
+        f_deviation.write(str(human) + " " + str(is_avg_by_people.get(human)) + "\n")
     for i in range(0, len(list(is_avg_by_people.keys()))):
         x = np.arange(0, len(is_avg_by_people.get(list(is_avg_by_people.keys())[i])), 1)
         y = np.array(is_avg_by_people.get(list(is_avg_by_people.keys())[i]))
@@ -51,7 +52,6 @@ def hypothesis_1_vse(error_guys):
 
 
 def hypothesis_1_ne_vse(error_guys):
-    global file
     avg_in_one_file = []
     more_than_avg = []
     more_than_avg_by_people = {}
@@ -75,6 +75,7 @@ def hypothesis_1_ne_vse(error_guys):
         avg_in_one_file = []
     print(more_than_avg_by_people)
 
+
 def reviewer_choice(prob_guys, commits_list):
     filenames = {}
     for user in prob_guys:
@@ -84,91 +85,87 @@ def reviewer_choice(prob_guys, commits_list):
             if filenames[filename][1] > prob_guys[user][filename]:
                 filenames[filename][0] = user
                 filenames[filename][1] = prob_guys[user][filename]
-    #for f in filenames:
-    #    print(f, filenames[f])
-        
     commits_reviewers = {}
-    
-    for commit in commits_list:
-        if commit[4] == True:
+    for single_commit in commits_list:
+        if single_commit[4]:
             continue
         reviewers = {}
         rev = ""
         val = 0
-        for files in commit[2]:
+        for files_commit in single_commit[2]:
             try:
-                if filenames[files][0] not in reviewers:
-                    reviewers[filenames[files][0]] = 1
+                if filenames[files_commit][0] not in reviewers:
+                    reviewers[filenames[files_commit][0]] = 1
                 else:
-                    reviewers[filenames[files][0]] += 1
+                    reviewers[filenames[files_commit][0]] += 1
             except Exception as exp:
+                print(exp)
                 pass
         for reviewer in reviewers:
             if reviewers[reviewer] > val:
                 val = reviewers[reviewer]
                 rev = reviewer
-        commits_reviewers[commit[3]] = rev
-
+        commits_reviewers[single_commit[3]] = rev
+    f_reviewers.write("Reviewers stats:\n\n")
     for i in commits_reviewers:
-        print(i, commits_reviewers[i])
+        f_reviewers.write(str(i) + " " + str(commits_reviewers[i]) + "\n")
+
 
 def prob(array, commits):
     prob_guys = {}
     for guy in array:
         for filename in array[guy]:
             error_prob = array[guy][filename][0] / (array[guy][filename][0] + array[guy][filename][1])
-
             if guy not in prob_guys:
                 prob_guys[guy] = {}
-
             prob_guys[guy][filename] = error_prob
-
     final_prob_with_commit = {}
-
-    for commit in commits:
-        sum = 1
-        if commit[0] not in prob_guys:
-            final_prob_with_commit[commit[3]] = 0.0
+    for single_commit in commits:
+        multiplier = 1
+        if single_commit[0] not in prob_guys:
+            final_prob_with_commit[single_commit[3]] = 0.0
             continue
-        for filename in commit[2]:
+        for filename in single_commit[2]:
             try:
-                sum *= (1 -(prob_guys[commit[0]][filename]))
+                multiplier *= (1 - (prob_guys[single_commit[0]][filename]))
             except Exception as exp:
+                print(exp)
                 pass
-        if commit[4] is True:
-            final_prob_with_commit["FIXED_" + commit[3].hexsha] = 1 - sum
+        if single_commit[4] is True:
+            final_prob_with_commit[single_commit[3].hexsha + " FIXED"] = 1 - multiplier
         else:
-            final_prob_with_commit[commit[3]] = 1 - sum
+            final_prob_with_commit[single_commit[3]] = 1 - multiplier
     f_probability.write("Probability stats:\n\n")
     for final in final_prob_with_commit:
-        f_probability.write(str(final) + " " + str(round(final_prob_with_commit[final] * 100, 4)) + "%\n")        
+        gap_prefix = " "
+        if final_prob_with_commit[final] * 100 >= 10:
+            gap_prefix = ""
+        f_probability.write(gap_prefix + str("{0:2.3f}".format((round(final_prob_with_commit[final] * 100, 4))))
+                            + "% " + str(final) + "\n")
     return prob_guys
-        
+
+
 def load_commits():
-    repo = Repo("D:\\selfPro\\becoder-hack\\rep2\\memos")
+    rep = Repo("D:\\selfPro\\becoder-hack\\rep2\\memos")
     commits_list = []
     error_guys = {}
-
-    master = repo.head.reference
-    commits = list(repo.iter_commits())
-    for commit in commits:
-
-        commit_data = [commit.author.email, commit.message.lower(), list(commit.stats.files.keys()), commit, False]
+    commits = list(rep.iter_commits())
+    for single_commit in commits:
+        commit_data = [single_commit.author.email, single_commit.message.lower(),
+                       list(single_commit.stats.files.keys()), single_commit, False]
         commits_list.append(commit_data)
     reviewer_commits_list = commits_list.copy()
 
-    for isfix in range(0, len(commits_list)-1):
-        if (commits_list[isfix][message].find("fix") != -1):
-
-            correctly_detected = []
-            fixed_files = commits_list[isfix][files].copy()
-            for i in range(isfix+1, len(commits_list)):
+    for is_fix in range(0, len(commits_list) - 1):
+        if commits_list[is_fix][message].find("fix") != -1:
+            fixed_files = commits_list[is_fix][files].copy()
+            for i in range(is_fix + 1, len(commits_list)):
                 if not fixed_files:
                     break
                 error_files = commits_list[i][files]
                 correctly_detected = list(set(fixed_files) & set(error_files))
-                for item in correctly_detected: 
-                    fixed_files.remove(item) 
+                for item in correctly_detected:
+                    fixed_files.remove(item)
 
                 if not correctly_detected:
                     continue
@@ -177,23 +174,22 @@ def load_commits():
                 for filename in correctly_detected:
                     if filename not in error_guys[commits_list[i][author]]:
                         commits_list[i][4] = True
-                        error_guys[commits_list[i][author]][filename] = [1,0]
+                        error_guys[commits_list[i][author]][filename] = [1, 0]
                     else:
-                        error_guys[commits_list[i][author]][filename][0]+=1
+                        error_guys[commits_list[i][author]][filename][0] += 1
         else:
-            user_changed_files = commits_list[isfix+1][files]
-            if commits_list[isfix+1][author] not in error_guys:
-                error_guys[commits_list[isfix+1][author]] = {}
+            user_changed_files = commits_list[is_fix + 1][files]
+            if commits_list[is_fix + 1][author] not in error_guys:
+                error_guys[commits_list[is_fix + 1][author]] = {}
             for filename in user_changed_files:
-                if filename not in error_guys[commits_list[isfix+1][author]]:
-                    error_guys[commits_list[isfix+1][author]][filename] = [0,1]
+                if filename not in error_guys[commits_list[is_fix + 1][author]]:
+                    error_guys[commits_list[is_fix + 1][author]][filename] = [0, 1]
                 else:
-                    error_guys[commits_list[isfix+1][author]][filename][1]+=1
-
-    #for check in error_guys:
+                    error_guys[commits_list[is_fix + 1][author]][filename][1] += 1
     f_start.write("Start stats:\n\n")
     for check in error_guys:
-        f_start.write(str(check) + " " + str(error_guys[check]))
+        f_start.write("--------------------------------------------\n")
+        f_start.write(str(check) + " " + str(error_guys[check]) + "\n")
     prob_guys = prob(error_guys, commits_list)
     reviewer_choice(prob_guys, reviewer_commits_list)
     hypothesis_1_vse(error_guys)
@@ -204,3 +200,4 @@ if __name__ == "__main__":
     f_probability.close()
     f_deviation.close()
     f_start.close()
+    f_reviewers.close()
