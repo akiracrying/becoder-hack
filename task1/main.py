f5a98d4fa24164708de87c9e6f5e6a22ae384638
Тимофей Цветков <tsvetkov.ti@edu.spbstu.ru>
2022-11-24 12:45:34 +0300

    [feature] Получил автора, сообщения, коммиты

--- task1/main.py
+++ task1/main.py
@@ -5,14 +5,18 @@ from git import *
 def load_commits():
 
     repo = Repo("C:\\Users\\Timon\Desktop\\becoder-hack\\task1\\knockout")
-    o = repo.remotes.origin
-    o.pull()
+    commits_list = []
 
     master = repo.head.reference
     commits = list(repo.iter_commits())
     for commit in commits:
-        print(commit.author, commit.message)
+        cur_files = commit.stats.files.keys()
+        commit_data = [commit.author, commit.message, list(commit.stats.files.keys())]
+        commits_list.append(commit_data)
+        #print(commit.author, commit.message, list(commit.stats.files.keys()))
+        #print(commits_list)
     
-
+    for i in commits_list:
+        print(i)
 if __name__ == "__main__":
     load_commits()   
